c39c0e250d0d5758f4c5225ea9790759415ee2bc
Martin Alexis Catoggio <martincatoggio2005@gmail.com>
2025-05-26 18:58:15 -0300

    agregar maquina

--- ApartadoMaquina/views.py
+++ ApartadoMaquina/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import MaquinaForm
+from .models import Maquina
+from django.db import IntegrityError
 
-# Create your views here.
+
+def agregar_maquina(request):
+    mensaje = ''
+    maquina_agregada = None
+
+    if request.method == 'POST':
+        form = MaquinaForm(request.POST, request.FILES)
+        if form.is_valid():
+            try:
+                maquina_agregada = form.save()
+                mensaje = '¡Maquinaria agregada correctamente!'
+                form = MaquinaForm()  # reiniciar el form vacío
+            except IntegrityError:
+                form.add_error('numero_de_serie', 'Ya existe una máquina con ese número de serie.')
+    else:
+        form = MaquinaForm()
+        
+    return render(request, 'ApartadoMaquina/formulario.html', {
+        'form': form,
+        'mensaje': mensaje,
+        'maquina_agregada': maquina_agregada
+    })
--- ManiMaquinas/settings.py
+++ ManiMaquinas/settings.py
@@ -11,11 +11,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -58,7 +60,7 @@ ROOT_URLCONF = "ManiMaquinas.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "es"
 
 TIME_ZONE = "UTC"
 
@@ -119,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
